655bb35881251bd026fbf3229d98767614961397
ilkerkonar <ilkerkonar@5ac06377-c52f-04e9-b5a8-6a64275c9dd1>
2014-06-01 09:47:57 +0000

    Devam

--- DesignPatternsGoogleApps/src/com/ilkerkonar/td/desen/ornekkodlar/observer/Satıcı.java
+++ DesignPatternsGoogleApps/src/com/ilkerkonar/td/desen/ornekkodlar/observer/Satıcı.java
@@ -15,9 +15,16 @@ public class Satıcı {
 
 	public static void main( final String[] args ) {
 
+		// Bir gözlem konusu yaratılıyor.
 		final FiyatKonusu konu = new FiyatKonusu();
+
+		// Gözlemci 1 yaratılıyor ve gözlem konusuna ekleniyor.
 		konu.gözlemciEkle( new Müşteri1() );
+
+		// Gözlemci 2 yaratılıyor ve gözlem konusuna ekleniyor.
 		konu.gözlemciEkle( new Müşteri2() );
+
+		// Gözlem konusundaki bir değişiklik, gözlemcilere haber veriliyor.
 		konu.fiyatDeğiştir( 50L );
 	}
 }
