6d7e8749800555d85754dee96ad13e9974eef461
softengilker <ilkerkonar@gmail.com>
2014-11-23 10:55:17 +0200

    TestBean and related files were added.

---
+++ schoolProject/backupdata/fk_make_cascade.sql
@@ -0,0 +1,42 @@
+﻿ALTER TABLE lesson DROP CONSTRAINT fk_lesson_classno;
+
+ALTER TABLE lesson
+  ADD CONSTRAINT fk_lesson_classno FOREIGN KEY (classno)
+      REFERENCES class (no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
+
+ALTER TABLE lesson DROP CONSTRAINT fk_lesson_teacherno;
+
+ALTER TABLE lesson
+  ADD CONSTRAINT fk_lesson_teacherno FOREIGN KEY (teacherno)
+      REFERENCES teacher (no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;      
+
+ALTER TABLE student DROP CONSTRAINT fk_student_classno;
+
+ALTER TABLE student
+  ADD CONSTRAINT fk_student_classno FOREIGN KEY (classno)
+      REFERENCES class (no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE; 
+
+ALTER TABLE test DROP CONSTRAINT fk_test_lessonno;
+
+ALTER TABLE test
+  ADD CONSTRAINT fk_test_lessonno FOREIGN KEY (lessonno)
+      REFERENCES lesson (no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
+
+ALTER TABLE teststudent DROP CONSTRAINT fk_teststudent_studentno;
+
+ALTER TABLE teststudent
+  ADD CONSTRAINT fk_teststudent_studentno FOREIGN KEY (studentno)
+      REFERENCES student (no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE; 
+
+ALTER TABLE teststudent DROP CONSTRAINT fk_teststudent_testno;
+
+ALTER TABLE teststudent
+  ADD CONSTRAINT fk_teststudent_testno FOREIGN KEY (testno)
+      REFERENCES test (no) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
+ 
