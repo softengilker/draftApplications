483770e4be324376a665ed554dbd3aa6b8bf33d9
softengilker <ilkerkonar@gmail.com>
2015-02-18 21:35:25 +0200

    do not care

--- SpringBootWsSample/src/test/java/demo/DemoApplicationTests.java
+++ SpringBootWsSample/src/test/java/demo/DemoApplicationTests.java
@@ -1,3 +1,4 @@
+
 package demo;
 
 import org.junit.Test;
@@ -5,8 +6,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = DemoApplication.class)
+@RunWith( SpringJUnit4ClassRunner.class )
+@SpringApplicationConfiguration( classes = DemoApplicationTests.class )
 public class DemoApplicationTests {
 
 	@Test
